824e152ce95838fc0acfd126bb66a1f9ff14c29c
namoo.jang <chrow16@naver.com>
2022-04-30 08:57:29 +0900

    init

---
+++ src/components/App.tsx
@@ -0,0 +1,18 @@
+import React, { useEffect, useState } from 'react'
+
+export const App: React.FC = () => {
+  const [count, setCount] = useState(0)
+  const [doubleCount, setDoubleCount] = useState(0)
+
+  useEffect(() => {
+    setDoubleCount(count * 2)
+  }, [count])
+
+  return (
+      <div>
+        <div>{count}</div>
+        <button onClick={() => setCount(count + 1)}>Add count</button>
+        <div>double count : {doubleCount}</div>
+      </div>
+  )
+}
